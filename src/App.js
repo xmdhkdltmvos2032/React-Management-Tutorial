3f717df1fdc741038c9f3fd70b9f84458bf7ef7f
jbh2032 <jbh2032@naver.com>
2023-08-29 20:21:26 +0900

    axios api

--- src/App.js
+++ src/App.js
@@ -9,21 +9,24 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import axios from "axios";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 
 
-function  App() {
 
-  const [data,setDate] = useState([]);
+function App() {
 
-  useEffect(()=>{
-    async function get(){
+  const [data, setDate] = useState([]);
+
+  useEffect(() => {
+    async function get() {
       const res = await axios.get("/api/hello");
-      if(res) {setDate(res.data)}
+      if (res) { setDate(res.data) }
     }
 
     get();
-  },[])
+  }, [])
 
 
 
@@ -31,7 +34,7 @@ function  App() {
 
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
-      <TableHead>
+        <TableHead>
           <TableRow>
             <TableCell>이름</TableCell>
             <TableCell align="right">걸그룹</TableCell>
@@ -41,7 +44,7 @@ function  App() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {data ? data.map((row) => (
+          {data && data.length > 0 ? data.map((row) => (
             <TableRow
               key={row.id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -54,7 +57,18 @@ function  App() {
               <TableCell align="right">{row.포지션}</TableCell>
               <TableCell align="right">{row.출생지}</TableCell>
             </TableRow>
-          )):""}
+          )) :
+
+            <TableRow>
+              <TableCell colSpan="6" align='center'>
+                <Box >
+                  <CircularProgress></CircularProgress>
+                </Box>
+              </TableCell>
+            </TableRow>
+
+
+          }
         </TableBody>
       </Table>
     </TableContainer>
